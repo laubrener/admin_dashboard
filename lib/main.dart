34ac12d03bf1af237c6727bd933bab6b2b1669db
laubrener <79546818+laubrener@users.noreply.github.com>
2025-12-19 11:25:37 -0300

    shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:admin_dashboard/providers/auth_provider.dart';
 import 'package:admin_dashboard/router/router.dart';
+import 'package:admin_dashboard/services/local_storage.dart';
 import 'package:admin_dashboard/ui/layouts/auth/auth_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  await LocalStorage.configurePrefs();
   Flurorouter.configureRoutes();
-  runApp(const MyApp());
+  runApp(const AppState());
+}
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => AuthProvider())],
+      child: MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +33,10 @@ class MyApp extends StatelessWidget {
       title: 'Admin Dashboard',
       initialRoute: Flurorouter.rootRoute,
       onGenerateRoute: Flurorouter.router.generator,
-      builder: (context, child) => AuthLayout(child: child!),
+      builder: (context, child) {
+        // print('Token: ${LocalStorage.preferences.getString('token')}');
+        return AuthLayout(child: child!);
+      },
       theme: ThemeData.light().copyWith(
         scrollbarTheme: ScrollbarThemeData(
           thumbColor: WidgetStateProperty.all(
